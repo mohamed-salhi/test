7ec5a4a19d94b01012863436056f97702ac2bd3d
محمد الصالحي <mohamed2562289mbn@gmail.com>
2025-08-21 16:46:25 +0300

    v3

--- app/Http/Controllers/Web/MainController.php
+++ app/Http/Controllers/Web/MainController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ads;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function home(){
+        $categories=Category::query()->with('posts')->get();
+        $tags=Tag::all();
+        $news=Post::all();
+        $latest=Post::query()->latest()->get();
+        $popular=Post::query()->orderByDesc('view')->get();
 
-        return view('web.home');
+        $ads=Ads::all();
+
+        return view('web.home',compact('popular','latest','categories','tags','news','ads'));
+    }
+    public function details($id){
+
+        $post=Post::query()->findOrFail($id);
+        $post->increment('view');
+        return view('web.details',compact('post'));
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Web\MainController;
 use Illuminate\Support\Facades\Route;
 Route::get('home',[MainController::class,'home']);
-Route::get('details',[MainController::class,'details']);
+Route::get('details/{id}',[MainController::class,'details'])->name('details');
 Route::get('contact',[MainController::class,'contact']);
 Route::post('contact',[MainController::class,'post']);
